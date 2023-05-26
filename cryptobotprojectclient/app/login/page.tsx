3199e05b1e9262169f26ea416961c3201f357320
bariskoklu <30669414+bariskoklu@users.noreply.github.com>
2023-05-26 22:12:28 +0300

    Changed home page and added the initial login page

--- cryptobotprojectclient/app/login/page.tsx
+++ cryptobotprojectclient/app/login/page.tsx
@@ -1,10 +1,10 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
-export default function Home() {
+export default function Login() {
   return (
     <main>
-      <h1>hello next 13 </h1>
+      <h1>login page </h1>
     </main>
   )
 }
